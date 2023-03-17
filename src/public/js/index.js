92c8a8042293650cc17e1f8e9f7348151e1aba95
Lindsay Garner <gar21034@byui.edu>
2023-03-16 20:36:14 -0600

    teetsing the api

---
+++ src/public/js/index.js
@@ -0,0 +1,35 @@
+// Testing the API
+
+const url = "https://lotr-api.onrender.com/characters";
+let place = document.querySelector("ul");
+let body = document.querySelector("body");
+
+async function updatePlace(url) {
+    const res = await fetch(url);
+
+    let data = await res.json();
+    console.log(data);
+    for(let i = 0; i < data.length; i++){
+        let name = data[i].name;
+        let race = data[i].race;
+        
+
+        let elements = document.createElement('li');
+        let element = document.createElement('p');
+
+        elements.innerHTML = name;
+        element.innerHTML = race;
+
+        place.appendChild(elements);
+        body.appendChild(element);
+
+
+    }
+    return place;
+}
+
+updatePlace(url).then(result => {
+    console.log(result);
+}).catch(error => {
+    console.log(error);
+});
--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,14 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "src/",
 
-  // build: {
-  //   outDir: "../dist",
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, "src/html/index.html"),
-  //       // cart: resolve(__dirname, "src/cart/index.html"),
-  //       // product1: resolve(__dirname, "src/product_pages/index.html"),
-  //       // product2: resolve(__dirname, "src/product_listing/index.html"),
-  //     },
-  //   },
-  // },
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/html/index.html"),
+        // product1: resolve(__dirname, "src/product_pages/index.html"),
+        // product2: resolve(__dirname, "src/product_listing/index.html"),
+      },
+    },
+  },
 });
