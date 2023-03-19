cca4a435c1ec29ad1e4b87c95c4b14678f55d45c
Lindsay Garner <gar21034@byui.edu>
2023-03-18 19:09:59 -0600

    zoom

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -37,4 +37,52 @@ updatePlace(url).then(result => {
 });
 
 
+// Zoom for the Map
+window.addEventListener("load", function() {
+    let container = document.getElementById("zoom-img");
+
+    // Get image src from the container element
+    var imageCss = window.getComputedStyle(container, false),
+        imageUrl = imageCss.backgroundImage
+                            .slice(4, -1).replace(/['"]/g, '');
+        
+    // Get the original source image
+    var imageSrc = new Image();
+    imageSrc.onload = function() {
+        var imageWidth = imageSrc.naturalWidth,
+            imageHeight = imageSrc.naturalHeight,
+            ratio = imageHeight / imageWidth;
+            console.log(imageWidth)
+
+        // Adjust the box to fit the image and to adapt responsively
+        var percentage = ratio * 100 + '%';
+        container.style.paddingBottom = percentage;
+
+        // Zoom and scan on mousemove
+        container.onmousemove = function(e) {
+        // Get the width of the thumbnail
+        var boxWidth = container.clientWidth,
+            // Get the cursor position, minus element offset
+            x = e.pageX - this.offsetLeft,
+            y = e.pageY - this.offsetTop,
+            // Convert coordinates to % of elem. width & height
+            xPercent = x / (boxWidth / 100) + '%',
+            yPercent = y / (boxWidth * ratio / 100) + '%';
+        // Update styles w/actual size
+        Object.assign(container.style, {
+            backgroundPosition: xPercent + ' ' + yPercent,
+            backgroundSize: imageWidth + 900 + 'px'
+        });
+        };
+        // Reset when mouse leaves
+        container.onmouseleave = function(e) {
+        Object.assign(container.style, {
+            backgroundPosition: 'center',
+            backgroundSize: 'cover'
+        });
+        };
+    }
+    imageSrc.src = imageUrl;
+})
+// end of zoom for the map
 
