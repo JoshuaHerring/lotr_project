9bb80447418ad152bd73fe06bf56adde00b64471
Lindsay Garner <gar21034@byui.edu>
2023-03-28 19:53:23 -0600

    secrets to show

--- src/public/js/hobbit.js
+++ src/public/js/hobbit.js
@@ -5,22 +5,51 @@ function getRace(param) {
     return race;
 }
 
+const raceTitle = document.getElementById('title-race');
 let race = getRace("race");
-console.log(typeof(race))
+raceTitle.innerHTML = race;
 
 
 
-async function getLocations(url){
+
+async function getStuff(url){
     let response  = await fetch(url);
     let characters = await response.json();
-    console.log(characters);
+    
     return characters;
 }
+
+async function showRaceSecrets(race){
+    const secretURL = "https://lotr-api.onrender.com/secrets";
+    const secretList = await getStuff(secretURL);
+    console.log(secretList)
+
+    secretList.forEach(secrets => {
+        // let stringedSecrets = JSON.stringify(secrets).replace(/"/g,'');
+        
+        if (secrets.race == race){
+            console.log(secrets.title)
+
+            const secretPlacement = document.getElementById('secrets');
+            const shh = document.createElement('div');
+
+            shh.innerHTML = `<h2>${secrets.title}</h2>
+            <p>${secrets.description}</p>
+            `;
+
+            secretPlacement.appendChild(shh);
+        }
+        
+    })
+
+};
+
+showRaceSecrets(race);
 
 
 async function showCharactersByRace(race) {
     let characterURL = "https://lotr-api.onrender.com/characters";
-    const characterList = await getLocations(characterURL);
+    const characterList = await getStuff(characterURL);
     console.log(characterList)
 
     characterList.forEach(character => {
@@ -29,8 +58,13 @@ async function showCharactersByRace(race) {
 
         if(cutRace == race){
             console.log(character.name)
-            const add = document.getElementById('add');
-            add.innerHTML = character.name;
+            const names = document.getElementById('names');
+
+            const info = document.createElement('p');
+
+            info.textContent = character.name;
+            names.appendChild(info);
+            // add.innerHTML = character.name;
         }
     });
 }
